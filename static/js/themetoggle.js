c70c2320b33e19783e198e201853a67e585ca94e
Matthias <26800596+not-matthias@users.noreply.github.com>
2024-04-23 16:23:39 +0200

    fix: theme selection (fixes #54)

--- static/js/themetoggle.js
+++ static/js/themetoggle.js
@@ -1,29 +1,42 @@
 function setTheme(mode) {
     localStorage.setItem("theme-storage", mode);
-    let htmlElement = document.querySelector("html");
-
-    if (mode === "dark") {
-        document.getElementById("darkModeStyle").disabled = false;
-        htmlElement.classList.remove("light")
-        htmlElement.classList.add("dark")
-
-        document.getElementById("sun-icon").style.display = "inline-block";
-        document.getElementById("moon-icon").style.display = "none";
-    } else if (mode === "light") {
-        document.getElementById("darkModeStyle").disabled = true;
-        htmlElement.classList.remove("dark")
-        htmlElement.classList.add("light")
-
-        document.getElementById("sun-icon").style.display = "none";
-        document.getElementById("moon-icon").style.display = "inline-block";
-    }
 }
+
+// Functions needed for the theme toggle
+// 
 
 function toggleTheme() {
     if (localStorage.getItem("theme-storage") === "light") {
         setTheme("dark");
+        updateItemToggleTheme();
     } else if (localStorage.getItem("theme-storage") === "dark") {
         setTheme("light");
+        updateItemToggleTheme();
+    }
+}
+
+function updateItemToggleTheme() {
+    let mode = getSavedTheme();
+
+    const darkModeStyle = document.getElementById("darkModeStyle");
+    if (darkModeStyle) {
+        darkModeStyle.disabled = (mode === "light");
+    }
+    
+    const sunIcon = document.getElementById("sun-icon");
+    const moonIcon = document.getElementById("moon-icon");
+    if (sunIcon && moonIcon) {
+        sunIcon.style.display = (mode === "dark") ? "inline-block" : "none";
+        moonIcon.style.display = (mode === "light") ? "inline-block" : "none";
+    }
+
+    let htmlElement = document.querySelector("html");
+    if (mode === "dark") {
+        htmlElement.classList.remove("light")
+        htmlElement.classList.add("dark")
+    } else if (mode === "light") {
+        htmlElement.classList.remove("dark")
+        htmlElement.classList.add("light")
     }
 }
 
@@ -40,4 +53,5 @@ function getSavedTheme() {
     return currentTheme;
 }
 
-setTheme(getSavedTheme());
+// Update the toggle theme on page load
+updateItemToggleTheme();
