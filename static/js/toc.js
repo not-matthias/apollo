f7a47bd6ca99a0762bd9ecd80444c3b5eb67c0a5
SeniorMars <cjh16@rice.edu>
2024-06-14 13:17:03 -0400

    that was not the right version of header.html

--- static/js/toc.js
+++ static/js/toc.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
+eocument.addEventListener('DOMContentLoaded', () => {
     const tocTitle = document.querySelector('.toc-title');
     const tocList = document.querySelector('.toc-list');
 
     tocTitle.addEventListener('click', function() {
-      // Toggle display of the ToC list
-      const isExpanded = tocList.style.display === 'block';
-      tocList.style.display = isExpanded ? 'none' : 'block';
+        // Toggle display of the ToC list
+        const isExpanded = tocList.style.display === 'block';
+        tocList.style.display = isExpanded ? 'none' : 'block';
 
-      // Toggle the class for rotating the arrow
-      if (isExpanded) {
-        tocTitle.classList.remove('expanded');
-      } else {
-        tocTitle.classList.add('expanded');
-      }
+        // Toggle the class for rotating the arrow
+        if (isExpanded) {
+            tocTitle.classList.remove('expanded');
+        } else {
+            tocTitle.classList.add('expanded');
+        }
     });
-  });
+});
