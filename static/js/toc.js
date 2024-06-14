18e1eb7a9cf5ddfbcf986d3964643aa572e88b84
SeniorMars <cjh16@rice.edu>
2024-06-14 17:06:26 -0400

    toc.js now does not give error if no toc is set

--- static/js/toc.js
+++ static/js/toc.js
@@ -2,16 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const tocTitle = document.querySelector('.toc-title');
     const tocList = document.querySelector('.toc-list');
 
-    tocTitle.addEventListener('click', function() {
-        // Toggle display of the ToC list
-        const isExpanded = tocList.style.display === 'block';
-        tocList.style.display = isExpanded ? 'none' : 'block';
+    if (tocTitle && tocList) {
+        tocTitle.addEventListener('click', function () {
+            const isExpanded = tocList.style.display === 'block';
+            tocList.style.display = isExpanded ? 'none' : 'block';
 
-        // Toggle the class for rotating the arrow
-        if (isExpanded) {
-            tocTitle.classList.remove('expanded');
-        } else {
-            tocTitle.classList.add('expanded');
-        }
-    });
+            if (isExpanded) {
+                tocTitle.classList.remove('expanded');
+            } else {
+                tocTitle.classList.add('expanded');
+            }
+        });
+    }
 });
