1f15ea4f70a63a8762832e66bde4f0328085474b
SeniorMars <cjh16@rice.edu>
2024-06-14 16:11:27 -0400

    make codeblock with line numbers look good

--- content/posts/markdown.md
+++ content/posts/markdown.md
@@ -10,6 +10,15 @@ tags=["blog"]
 
 +++
 
+# H1
+## H2
+### H3
+
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet sagittis id consectetur purus ut. In pellentesque massa placerat duis ultricies. Neque laoreet suspendisse interdum consectetur libero id. Justo nec ultrices dui sapien eget mi proin. Nunc consequat interdum varius sit amet mattis vulputate. Sollicitudin tempor id eu nisl nunc mi ipsum. Non odio euismod lacinia at quis. Sit amet nisl suscipit adipiscing. Amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. Sit amet consectetur adipiscing elit pellentesque habitant. Ac placerat vestibulum lectus mauris. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. [Google](https://www.google.com)
+
+![Markdown Logo](https://markdown-here.com/img/icon256.png)
+
 ## Code Block
 
 ```rust
@@ -18,11 +27,59 @@ fn main() {
 }
 ```
 
+
+```rust,hl_lines=2,linenos
+fn main() {
+    println!("Hello World");
+}
+```
+
+## Ordered List
+
+1. First item
+2. Second item
+3. Third item
+
+## Unordered List
+
+- List item
+- Another item
+- And another item
+
+## Nested list
+
+- Fruit
+  - Apple
+  - Orange
+  - Banana
+- Dairy
+  - Milk
+  - Cheese
+
 ## Quote
 
-> Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.
-- Albert Einstein
+> Two things are infinite: the universe and human stupidity; and I'm not sure about the
+> universe.<br>
+> — <cite>Albert Einstein</cite>
 
+
+## Table Inline Markdown
+
+| Italics   | Bold     | Code   | StrikeThrough     |
+| --------  | -------- | ------ | ----------------- |
+| *italics* | **bold** | `code` | ~~strikethrough~~ |
+
+## Foldable Text
+
+<details>
+    <summary>Title 1</summary>
+    <p>IT'S A SECRET TO EVERYBODY.</p>
+</details>
+
+<details>
+    <summary>Title 2</summary>
+    <p>Stay awhile, and listen!</p>
+</details>
 
 ## Code tags
 
--- static/js/clipboard.js
+++ static/js/clipboard.js
@@ -1,37 +1,59 @@
+const successIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-check-lg" viewBox="0 0 16 16">
+            <path d="M13.485 1.85a.5.5 0 0 1 1.065.02.75.75 0 0 1-.02 1.065L5.82 12.78a.75.75 0 0 1-1.106.02L1.476 9.346a.75.75 0 1 1 1.05-1.07l2.74 2.742L12.44 2.92a.75.75 0 0 1 1.045-.07z"/>
+        </svg>`;
+const errorIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-x-lg" viewBox="0 0 16 16">
+            <path d="M2.293 2.293a1 1 0 0 1 1.414 0L8 6.586l4.293-4.293a1 1 0 0 1 1.414 1.414L9.414 8l4.293 4.293a1 1 0 0 1-1.414 1.414L8 9.414l-4.293 4.293a1 1 0 0 1-1.414-1.414L6.586 8 2.293 3.707a1 1 0 0 1 0-1.414z"/>
+        </svg>`;
+const copyIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-clipboard" viewBox="0 0 16 16">
+                <path d="M10 1.5a.5.5 0 0 1 .5-.5h2a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2h-9a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h2a.5.5 0 0 1 .5.5V3h3V1.5zM6.5 3V2h3v1h-3zm4 0v1h2a1 1 0 0 0-1-1h-2V3zm-5 0H3a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h9a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H5.5V3z"/>
+            </svg>`;
+
+// Function to change icons after copying
+const changeIcon = (button, isSuccess) => {
+    button.innerHTML = isSuccess ? successIcon : errorIcon;
+    setTimeout(() => {
+        button.innerHTML = copyIcon; // Reset to copy icon
+    }, 2000);
+};
+
+// Function to get code text from tables, skipping line numbers
+const getCodeFromTable = (codeBlock) => {
+    return [...codeBlock.querySelectorAll('tr')]
+        .map(row => row.querySelector('td:last-child')?.innerText ?? '')
+        .join('');
+};
+
+// Function to get code text from non-table blocks
+const getNonTableCode = (codeBlock) => {
+    return codeBlock.textContent.trim();
+};
+
 document.addEventListener('DOMContentLoaded', function() {
-  // Select all `pre` elements containing `code`
-  var codeBlocks = document.querySelectorAll('pre code');
 
-  codeBlocks.forEach(function(code) {
-    // Create a new button element for copying
-    var copyBtn = document.createElement('button');
-    copyBtn.className = 'clipboard-button';
-    // Set the initial SVG for copying
-    copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ef5350" class="bi bi-clipboard" viewBox="0 0 16 16">
-  <path d="M10 1.5a.5.5 0 0 1 .5-.5h2a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2h-9a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h2a.5.5 0 0 1 .5.5V3h3V1.5zM6.5 3V2h3v1h-3zm4 0v1h2a1 1 0 0 0-1-1h-2V3zm-5 0H3a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h9a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H5.5V3z"/>
-</svg>`;
-    copyBtn.setAttribute('aria-label', 'Copy code to clipboard');
+    // Select all `pre` elements containing `code`
+    document.querySelectorAll('pre code').forEach(codeBlock => {
+        const copyBtn = document.createElement('button');
+        copyBtn.className = 'clipboard-button';
+        copyBtn.innerHTML = copyIcon;
+        copyBtn.setAttribute('aria-label', 'Copy code to clipboard');
 
-    // Add the copy functionality
-    copyBtn.addEventListener('click', function() {
-      navigator.clipboard.writeText(code.textContent).then(() => {
-        // Change to checkmark icon to indicate successful copying
-        copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ef5350" class="bi bi-check-lg" viewBox="0 0 16 16">
-  <path d="M13.485 1.85a.75.75 0 0 1 1.065.02.75.75 0 0 1-.02 1.065L5.82 12.78a.75.75 0 0 1-1.106.02L1.476 9.346a.75.75 0 1 1 1.05-1.07l2.74 2.742L12.44 2.92a.75.75 0 0 1 1.045-.07z"/>
-</svg>`;
-        setTimeout(() => {
-          // Reset to copy icon after 2 seconds
-          copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ef5350" class="bi bi-clipboard" viewBox="0 0 16 16">
-  <path d="M10 1.5a.5.5 0 0 1 .5-.5h2a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2h-9a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h2a.5.5 0 0 1 .5.5V3h3V1.5zM6.5 3V2h3v1h-3zm4 0v1h2a1 1 0 0 0-1-1h-2V3zm-5 0H3a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h9a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H5.5V3z"/>
-</svg>`;
-        }, 2000);
-      }).catch(err => {
-        console.error('Failed to copy text: ', err);
-      });
+        // Attach event listener to copy button
+        copyBtn.addEventListener('click', async () => {
+            // Determine if the code is in a table or not
+            const isTable = codeBlock.querySelector('table');
+            const codeToCopy = isTable ? getCodeFromTable(codeBlock) : getNonTableCode(codeBlock);
+
+            try {
+                await navigator.clipboard.writeText(codeToCopy);
+                changeIcon(copyBtn, true); // Show success icon
+            } catch (error) {
+                console.error('Failed to copy text: ', error);
+                changeIcon(copyBtn, false); // Show error icon
+            }
+        });
+
+        // Append the button to the parent `pre` element, not inside the `code`
+        codeBlock.parentNode.style.position = 'relative'; // Ensure parent `pre` can contain the absolute button
+        codeBlock.parentNode.appendChild(copyBtn);
     });
-
-    // Append the button to the parent `pre` element, not inside the `code`
-    code.parentNode.style.position = 'relative'; // Make sure parent `pre` can contain the absolute button
-    code.parentNode.appendChild(copyBtn);
-  });
 });
