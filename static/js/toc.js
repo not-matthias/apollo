5fbbb801a8a9b58a2d6a698c7b4f05443885aa3c
SeniorMars <cjh16@rice.edu>
2024-06-15 02:22:21 -0400

    added toc javascript to hover
    that was not the right version of header.html

---
+++ static/js/toc.js
@@ -0,0 +1,17 @@
+eocument.addEventListener('DOMContentLoaded', () => {
+    const tocTitle = document.querySelector('.toc-title');
+    const tocList = document.querySelector('.toc-list');
+
+    tocTitle.addEventListener('click', function() {
+        // Toggle display of the ToC list
+        const isExpanded = tocList.style.display === 'block';
+        tocList.style.display = isExpanded ? 'none' : 'block';
+
+        // Toggle the class for rotating the arrow
+        if (isExpanded) {
+            tocTitle.classList.remove('expanded');
+        } else {
+            tocTitle.classList.add('expanded');
+        }
+    });
+});
