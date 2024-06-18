b03f9e0c5f8ec6db2fa8ae7cba2f9380ac9d673d
SeniorMars <cjh16@rice.edu>
2024-06-18 10:10:16 -0400

    fixed toc, codeblock, and css bugs. added a couple more features

--- static/js/toc.js
+++ static/js/toc.js
@@ -3,15 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const tocList = document.querySelector('.toc-list');
 
     if (tocTitle && tocList) {
-        tocTitle.addEventListener('click', function () {
-            const isExpanded = tocList.style.display === 'block';
+        const toggleToC = () => {
+            const isExpanded = tocList.style.display === 'block' || window.getComputedStyle(tocList).display === 'block';
             tocList.style.display = isExpanded ? 'none' : 'block';
+            tocTitle.classList.toggle('expanded', !isExpanded);
+        };
 
-            if (isExpanded) {
-                tocTitle.classList.remove('expanded');
-            } else {
-                tocTitle.classList.add('expanded');
-            }
-        });
+        tocTitle.addEventListener('click', toggleToC);
     }
 });
+
