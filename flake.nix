24d5a2198fbffcbb1983a58f5a872bbf652d8376
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-10-04 22:40:31 +0200

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Apollo - A modern and minimalistic blog theme for Zola";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          zola
+          pre-commit
+
+          # Formatters
+          treefmt
+          nodePackages.prettier
+          alejandra
+          djlint
+        ];
+
+        shellHook = ''
+          # Install pre-commit hooks if not already installed
+          if [ ! -f .git/hooks/pre-commit ]; then
+            echo "Installing pre-commit hooks..."
+            pre-commit install
+          fi
+        '';
+      };
+    });
+}
