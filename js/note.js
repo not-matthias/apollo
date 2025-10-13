9bc0c3dc678c25938b6466009021e3a100f9b6a6
not-matthias <not-matthias@users.noreply.github.com>
2025-10-13 20:38:12 +0000

    deploy: 7e92736fc4581550a56f2d5c249d7cd24dc72d7e

--- js/note.js
+++ js/note.js
@@ -1,14 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.note-toggle').forEach(function(toggleButton) {
-    var content = toggleButton.nextElementSibling;
-    var isHidden = content.style.display === 'none';
-    toggleButton.setAttribute('aria-expanded', !isHidden);
-
-    toggleButton.addEventListener('click', function() {
-      var expanded = this.getAttribute('aria-expanded') === 'true';
-      this.setAttribute('aria-expanded', !expanded);
-      content.style.display = expanded ? 'none' : 'block';
-    });
-  });
-});
-
+document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll(".note-toggle").forEach(function(e){var t=e.nextElementSibling,n=t.style.display==="none";e.setAttribute("aria-expanded",!n),e.addEventListener("click",function(){var e=this.getAttribute("aria-expanded")==="true";this.setAttribute("aria-expanded",!e),t.style.display=e?"none":"block"})})})
